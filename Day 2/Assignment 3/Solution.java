4e7a37ed4ea1c40b9c5325b53ad7c7c7f1bccdd3
SVBhuvanChandra <S.V.BhuvanChandra@msitprogram.net>
2019-10-30 07:46:52 +0530

    enlosed final

---
+++ Day 2/Assignment 3/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class Solution{
+	public static String isMatching(String str){
+		// fill you code Here
+		int flag1 = 2;
+		int flag2 = 2;
+		int flag3 = 2;
+		char[] arr = str.toCharArray();
+		Stack <Character> a = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
+				a.push(arr[i]);
+			} else {
+				if (a.isEmpty()  == false && arr[i] == ')') {
+					if (a.pop() == '(') {
+						flag1 = 1;
+					} else {
+						flag1 = 0;
+						break;
+					}
+				} else if (a.isEmpty()  == false && arr[i] == ']') {
+					if (a.pop() == '[') {
+						flag2 = 1;
+					} else {
+						flag2 = 0;
+						break;
+					}
+				} else if (a.isEmpty()  == false && arr[i] == '}') {
+					if (a.pop() == '{') {
+						flag3 = 1;
+					} else {
+						flag3 = 0;
+						break;
+					}
+				}
+			}
+		}
+		if (flag1 != 0 && flag2 != 0 && flag3 != 0) {
+			return "YES";
+		} else {
+			return "NO";
+		}
+	}
+}
