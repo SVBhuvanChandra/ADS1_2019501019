1efe1aef0a1ee452065cbfeff8eb4deea004a374
SVBhuvanChandra <S.V.BhuvanChandra@msitprogram.net>
2019-11-02 12:08:57 +0530

    All test cases passed

--- Day 3/Assignment 3/Solution.java
+++ Day 3/Assignment 3/Solution.java
@@ -1,20 +1,55 @@
+/**
+ * This is soultion class, the implementaions of Select and Insertion sorts.
+ */
 class Solution{
-	public int[] sortInsertion(int[] arr) {
-		// fill you code Here
-
-		return null;
+	/**
+	 * Insertion sort implementation method and two two loops are
+	 * iterating and hence the time complexity is O(n2) [n square].
+	 * @param arr is the array to be sorted.
+	 * @return a sorted array.
+	 */
+    public int[] sortInsertion(int[] arr){
+		// Outer loop runnning array length times.
+        for(int i = 0; i < arr.length; i++) {
+			// inner loop running i times.
+            for(int j = i; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j , j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
 	}
-	public int[] sortSelection(int[] arr) {
-		// fill you code Here
-		if (arr.length != 0) {
-			int min = arr[0];
-			for (int i = 0; i < arr.length; i++) {
-				if (min > arr[i]) {
-					int temp = arr[i];
-					arr[i] = min;
-					min = temp;
-				}
-			}
-		}
+	/**
+	 * Selection sort implementation and two loops are iterating
+	 * and hence the time complexity is O(n2) [n square].
+	 * @param arr is the array to be sorted.
+	 * @return a sorted array.
+	 */
+    public int[] sortSelection(int[] arr){
+        // Outer loop runnning array length times.
+        for(int i =0; i < arr.length; i++) {
+			int min = i;
+			// inner loop running i to array length times.
+            for(int j =i+1; j< arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            } swap(arr, min, i);
+        }
+        return arr;
 	}
+	/**
+	 * This is swapping funtion.
+	 * @param arr is the input array.
+	 * @param i iterating variable from outer loop.
+	 * @param j iterating variable from inner loop.
+	 */
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
