6e7acb6ed034c95e5b02901bd2c464f8bbca8e26
SVBhuvanChandra <S.V.BhuvanChandra@msitprogram.net>
2019-11-03 08:46:46 +0530

    Modified

--- Day 2/Assignment 4/Solution.java
+++ Day 2/Assignment 4/Solution.java
@@ -1,19 +1,36 @@
 import java.util.*;
+/**
+ * Solutuon for josephus problem.
+ * @author Bhuvan.
+ */
 class Solution{
+	/**
+	 * Method josephus() will take inputs in the linked
+	 * and performs queue operations.
+	 * @param a input1
+	 * @param b input2
+	 * @return String 
+	 */
 	public static String Josephus(int a, int b){
-	String s = "";
-	Queue<Integer> queue = new LinkedList<>();
-	for (int t = 0; t < a; t++) {
-	    queue.add(t);
-	}
-	while (queue.size() > 1) {
-	    for (int k = 0; k < b-1; k++) {
-	        int a1 = queue.remove();
-	        queue.add(a1);
-	    }
-	    s = s + queue.remove() + " ";
-	}
-	s = s+queue.remove();
-	return s;
+		/**
+		 * Field to hold output string.
+		 */
+		String s = "";
+		/**
+		 * Implementing the Queue using linked lists. 
+		 */
+		Queue<Integer> queue = new LinkedList<>();
+		for (int t = 0; t < a; t++) {
+			queue.add(t);
+		}
+		while (queue.size() > 1) {
+			for (int k = 0; k < b-1; k++) {
+				int a1 = queue.remove();
+				queue.add(a1);
+			}
+			s = s + queue.remove() + " ";
+		}
+		s = s+queue.remove();
+		return s;
 	}
 }
