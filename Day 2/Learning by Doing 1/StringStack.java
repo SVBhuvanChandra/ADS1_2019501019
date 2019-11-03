1d0c4b0c5071bc3a7b06c2b09d97311701f172cb
SVBhuvanChandra <S.V.BhuvanChandra@msitprogram.net>
2019-11-03 08:51:58 +0530

    Modified.

--- Day 2/Learning by Doing 1/StringStack.java
+++ Day 2/Learning by Doing 1/StringStack.java
@@ -1,9 +1,26 @@
 import java.io.*;
 import java.util.*;
+/**
+ * StringStack class to perform stack methods.
+ * @author Bhuvan.
+ */
 public class StringStack {
+    /**
+     * Main method implementation.
+     * @param args input parameters.
+     */
     public static void main(String[] args) {
+        /**
+         * Input string.
+         */
         String s = "it was - the best - of times - - - it was - the - -";
+        /**
+         * Implementening the Stack data structure.
+         */
         Stack <String> a = new Stack<>();
+        /**
+         * Field to hold string array.
+         */
         String[] sa = s.split(" ");
         for (int i = 0; i < sa.length; i++) {
             if (sa[i].equals("-")) {
